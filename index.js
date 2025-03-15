565a8a17bf22877485ce0d1bfff3d18650063c10
Apurv Pandey <apurvpandey1306@gmail.com>
2025-03-16 02:26:52 +0530

    did the html and css

---
+++ index.js
@@ -0,0 +1,15 @@
+// Weight (Mass)
+// 1 kilogram (kg) = 2.20462 pounds (lbs)
+// 1 pound (lb) = 0.453592 kilograms (kg)
+
+// Volume
+// 1 liter (L) = 0.264172 gallons (US)
+// 1 gallon (US) = 3.78541 liters (L)
+
+// Length
+// 1 meter (m) = 3.28084 feet (ft)
+// 1 foot (ft) = 0.3048 meters (m)
+
+function render(val, unit) {
+  let retVal = "";
+}
